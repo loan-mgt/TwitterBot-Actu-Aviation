e03a74d3526b0e2f0eb0bb3f12e74771f0f5b0ff
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2021-11-26 16:43:21 +0100

    Add files via upload

---
+++ AIRCOSMO.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_news():
+	resul = {}
+
+	r = requests.get("https://www.air-cosmos.com/actualite/aviation-civile")
+
+
+	b = BeautifulSoup(r.text,features="html5lib")
+
+	r = b.findAll('div', attrs={"class":"col-md-6"})
+
+	for i in r:
+
+		if i.findAll('span', attrs={"style":"color: inherit;"}) == [] and len(i.findAll('h2', attrs={"class":"title"})) == 1:
+			m = i.findAll('h2', attrs={"class":"title"})[0].text +'\n'+ i.findAll('p', attrs={"class":"headline"})[0].text
+			img = i.findAll('div')[0]["style"][22:-2]
+			resul.append({'title':m,'image':img})
+			
