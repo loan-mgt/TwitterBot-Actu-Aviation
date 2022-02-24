ea0e951b76bfe75f55d9b2dbdd543d058a6d2ccb
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2022-02-24 13:05:16 +0100

    Update Key.py

--- Key.py
+++ Key.py
@@ -1,8 +1,8 @@
-tweet_key = "B3Thu92kkZGxEayzd5wKvXUHj1"
-tweet_secrete = "swynkgvWHRCWGVa2QtQRt4bRNZAgkjQ5f3SxeSkG33QmUuVtIk2"
-tweet_token ="1439997163552002056-ztFhLiFsgkvh2QNq44WM6UjtebxKbd3"
-tweet_token_secrete = "fuisCQu2Wo4VbnvA4VTJQiMQgqzym4zf7EkP9yRu2XJiO4"
-deepl_key = "af71aef7-f83a-8940-e7b6-a109096d252c:fx5"
+tweet_key = ""
+tweet_secrete = ""
+tweet_token =""
+tweet_token_secrete = ""
+deepl_key = ""
 
 
 
